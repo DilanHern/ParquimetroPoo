d079aca454fda7ff64c1b848e354a390cefce697
DilanHern <dilanhernandez48@gmail.com>
2024-10-03 14:21:34 -0600

    Añadiendo proyecto NetBeans

---
+++ src/main/java/com/tec/parquimetro/parquimetro/Parquimetro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tec.parquimetro.parquimetro;
+
+/**
+ *
+ * @author Dilan
+ */
+public class Parquimetro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
