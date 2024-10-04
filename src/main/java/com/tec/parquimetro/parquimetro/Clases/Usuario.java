c838fc1e5d97ab30ca482f257c1fc9748583b76d
Carol Araya <carayacn@gmail.com>
2024-10-03 18:33:30 -0600

    Creación de carpetas y archivos clases
    Persona
    Usuario
    Inspector
    Administrador
    Login

---
+++ src/main/java/com/tec/parquimetro/parquimetro/Clases/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tec.parquimetro.parquimetro.Clases;
+
+/**
+ *
+ * @author carol_flgngfy
+ */
+public class Usuario {
+    
+}
